881082812f43c5b6ddfdba8e50013ccaa6a79710
Bartosz <bartoszmrosek@op.pl>
2022-09-27 19:10:29 +0200

    Small refactor

--- backend/src/queries.ts
+++ backend/src/queries.ts
@@ -124,7 +124,7 @@ export class DbQueries {
       return new Promise((resolve, reject) => {
         connection.execute<userInfoWithPacket[]>(
           'SELECT user_id, username FROM user_accounts WHERE username LIKE ?',
-          username,
+          [username],
           (err, res) => {
             if (err) reject(err);
             resolve(res);
@@ -153,7 +153,7 @@ export class DbQueries {
     OR
     (user_messages.sender_user_id = ? AND user_messages.reciever_user_id = user_accounts.user_id)
     ORDER BY user_messages.created_at ASC`,
-          userId,
+          [userId],
           (err, res) => {
             if (err) reject(err);
             resolve(res);
