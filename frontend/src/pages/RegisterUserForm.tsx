d84c47a70983dcc411b0cac484e6752d65511f94
Bartosz <bartoszmrosek@op.pl>
2022-09-30 15:53:51 +0200

    More work on form handling, start of refactoring to controlled inputs

--- frontend/src/pages/RegisterUserForm.tsx
+++ frontend/src/pages/RegisterUserForm.tsx
@@ -1,5 +1,5 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
-import React, { ReactElement, useContext, useEffect, useState } from 'react';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import React, { useContext, useEffect, useState } from 'react';
 import { SocketContext } from '../Contexts/SocketContext';
 import { standardDbResponse } from '../interfaces/dbResponsesInterface';
 import ErrorOverlay from '../components/ErrorOverlay';
@@ -21,8 +21,9 @@ const RegisterUserForm = () => {
   const standardSocket: Socket<ServerToClientEvents, ClientToServerEvents> =
     useContext(SocketContext);
   const {
-    register,
     handleSubmit,
+    control,
+    reset,
     formState: { errors },
   } = useForm<userInput>();
   const [isSubmitSuccessfull, setIsSubmitSuccessfull] = useState<
@@ -88,12 +89,14 @@ const RegisterUserForm = () => {
         ></span>
       </span>
       <form className="grid text-center gap-5 z-0 absolute font-medium">
+        <Controller name='username' control={control} defaultValue='' rules={{required: true}} render={()}/>
         <label className="block">
           <input
-            className={`peer transition-all duration-500 p-3 rounded-md ring-1 ring-offset-1 ring-gray-400/80
-             hover:ring-gray-600 outline-main-purple/80 overflow-hidden
-              outline-0 outline-offset-1 ${
-                errors.username && 'outline-1 outline-red-600'
+            className={`peer transition-all duration-500 p-3 rounded-md ring-offset-1 ring-gray-400/80
+              ring-1 outline-offset-2 outline-3 ${
+                errors.username
+                  ? ' outline-red-600 ring-red-600'
+                  : ' hover:ring-gray-600 focus:outline-main-purple/80'
               }`}
             type="text"
             {...register('username', {
@@ -105,30 +108,17 @@ const RegisterUserForm = () => {
           <span
             className={`transtion-all duration-500 absolute left-3 top-3
             ${
-              errors.username &&
-              'text-red-600 scale-[85%] translate-x-[-12.5%] translate-y-[-100%] bg-white px-1'
+              errors.username
+                ? 'text-red-600 translate-x-[-12.5%] translate-y-[-100%] scale-[85%] px-1'
+                : 'text-black/50 peer-focus:text-main-purple'
             }
-             text-black/50 pointer-events-none
-           peer-focus:scale-[85%] peer-focus:translate-x-[-12.5%] peer-focus:translate-y-[-100%] peer-focus:text-main-purple
-            peer-focus:bg-white rounded-full peer-focus:px-1 `}
+            pointer-events-none
+           peer-focus:scale-[85%] peer-focus:translate-x-[-12.5%] peer-focus:translate-y-[-100%]
+            bg-white rounded-full peer-focus:px-1 `}
           >
-            Username
+            {!errors.username ? 'Username' : 'Field is required'}
           </span>
         </label>
-        <input
-          className=""
-          type="email"
-          {...register('email', { required: 'email is required' })}
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          className=""
-          type="password"
-          {...register('password', { required: 'password is required' })}
-          name="password"
-          placeholder="Password"
-        />
         <button onClick={handleSubmit(onSubmit)}>Submit</button>
         {isSubmitSuccessfull && <p>Submit succesfull</p>}
         {error !== null && <ErrorOverlay error={error} />}
