647658e019545e3348747ccbdcb8e2664f57e872
Bartosz <bartoszmrosek@op.pl>
2022-10-19 18:36:09 +0200

    UI changes to navbar on mobile and new media hook

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import {
   ServerToClientEvents,
   ClientToServerEvents,
 } from '../interfaces/socketContextInterfaces';
+import useMedia from '../hooks/useMedia';
 
 const Navbar = ({ shouldRender }: { shouldRender: boolean }) => {
   const { user, removeUser }: UserContextExports = useContext(UserContext);
@@ -17,6 +18,7 @@ const Navbar = ({ shouldRender }: { shouldRender: boolean }) => {
   const [searchParameters, setSearchParameters] = useState<string>('');
   const navigate = useNavigate();
   const searchRef = useRef<HTMLInputElement>(null);
+  const media = useMedia();
 
   useEffect(() => {
     searchRef.current?.setCustomValidity('');
@@ -38,7 +40,7 @@ const Navbar = ({ shouldRender }: { shouldRender: boolean }) => {
   };
 
   const shouldRenderUser = () => {
-    if (user !== undefined && user !== null) {
+    if (user !== undefined && user !== null && media !== 'sm') {
       return (
         <li>
           <h1>Username: {user.username}</h1>
@@ -50,11 +52,7 @@ const Navbar = ({ shouldRender }: { shouldRender: boolean }) => {
 
   const shouldRenderNewListItems = () => {
     if (user !== undefined && user !== null) {
-      return (
-        <li>
-          <NavLink to="Messeges">Messeges</NavLink>
-        </li>
-      );
+      return <NavLink to="Messeges">Messeges</NavLink>;
     }
   };
 
@@ -67,54 +65,56 @@ const Navbar = ({ shouldRender }: { shouldRender: boolean }) => {
 
   return (
     <nav
-      className={`z-10 fixed w-screen mb-5 ${
+      className={`z-10 fixed bottom-0 md:top-0 w-screen h-fit md:first-letter:mb-5 ${
         !shouldRender && 'hidden md:block'
       }`}
     >
-      <section
-        className="grid grid-cols-2 grid-rows-2 gap-3 items-center
+      <div
+        className="grid grid-cols-2 grid-rows-1 gap-3 items-center
        justify-end md:flex md:flex-row m-5 font-semibold text-[#371965]
         text-center"
       >
-        <NavLink
-          to="Register"
-          className="transition duration-1000 p-1 md:p-2 rounded-2xl bg-[#EBECED] border-4
-         hover:border-green-400 hover:border-solid hover:border-4 md:bg-main-purple md:text-[#EBECED] w-3/5 md:min-w-[6rem] md:w-[10%] lg:w-[6%] justify-self-center md:order-2"
-        >
-          Register
-        </NavLink>
         {!user ? (
-          <NavLink
-            to="Login"
-            className="transition duration-1000 p-1 md:p-2 rounded-2xl border-4 border-[#ad79fd] hover:border-green-400 w-3/5 md:min-w-[6rem] md:w-[10%] lg:w-[6%] justify-self-center md:order-last"
-          >
-            Login
-          </NavLink>
+          <>
+            <NavLink
+              to="Register"
+              className="transition duration-1000 p-1 md:p-2 rounded-2xl border-4
+         hover:border-green-400 hover:border-solid hover:border-4 bg-main-purple text-[#EBECED] w-3/5 md:min-w-[6rem] md:w-[10%] lg:w-[6%] justify-self-center md:order-2"
+            >
+              Register
+            </NavLink>
+            <NavLink
+              to="Login"
+              className="transition duration-1000 p-1 md:p-2 rounded-2xl border-4 border-[#ad79fd] bg-porcelain hover:border-green-400 w-3/5 md:min-w-[6rem] md:w-[10%] lg:w-[6%] justify-self-center md:order-last"
+            >
+              Login
+            </NavLink>
+          </>
         ) : (
-          <button
-            className="transition duration-1000 p-1 md:p-2 rounded-2xl border-4 border-[#ad79fd] hover:border-green-400 w-3/5 md:min-w-[6rem] md:w-[10%] lg:w-[6%] justify-self-center md:order-last"
-            onClick={logoutUser}
-          >
-            Logout
-          </button>
-        )}
-        <div className="col-span-2">
-          <form onSubmit={handleSearchSubmit}>
-            <input
-              className="transition duration-1000 p-2 md:p-3 rounded-full focus:outline-none focus:ring
+          <>
+            <button
+              className="transition duration-1000 p-1 md:p-2 rounded-2xl border-4 border-[#ad79fd] hover:border-green-400 w-3/5 md:min-w-[6rem] md:w-[10%] lg:w-[6%] justify-self-center md:order-last"
+              onClick={logoutUser}
+            >
+              Logout
+            </button>
+            <form onSubmit={handleSearchSubmit}>
+              <input
+                className="transition duration-1000 p-2 md:p-3 rounded-full focus:outline-none focus:ring
                focus:ring-main-purple/50 invalid:focus:ring-red-600 border-2 hover:border-main-purple invalid:hover:border-red-600"
-              type="search"
-              name="search-params"
-              value={searchParameters}
-              onChange={handleChange}
-              placeholder={'Search'}
-              ref={searchRef}
-            />
-          </form>
-        </div>
-      </section>
+                type="search"
+                name="search-params"
+                value={searchParameters}
+                onChange={handleChange}
+                placeholder={'Search'}
+                ref={searchRef}
+              />
+            </form>
+          </>
+        )}
+        {shouldRenderNewListItems()}
+      </div>
       {shouldRenderUser()}
-      {shouldRenderNewListItems()}
     </nav>
   );
 };
