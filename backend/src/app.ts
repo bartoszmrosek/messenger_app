dfdf73494f60c5c2a589badc3ff45ef431478277
Bartosz <bartoszmrosek@op.pl>
2022-11-30 23:29:16 +0100

    Cleaned unused typescript rule overrides

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,3 @@
-/* eslint-disable  @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-
 import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
