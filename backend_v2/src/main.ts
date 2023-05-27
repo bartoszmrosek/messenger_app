7be039235effcb58794ccb93378a37ccc2a2d0c5
Bartosz <bartoszmrosek@op.pl>
2023-05-27 12:09:33 +0200

    Added global middlewares, updated styling

--- backend_v2/src/main.ts
+++ backend_v2/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import helmet from "helmet";
+
+const CORS_OPTIONS = {
+	credentials: true,
+	origin:
+		process.env.NODE_ENV === "development" ? "http://localhost:5173/" : "",
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.use(helmet());
+	app.enableCors(CORS_OPTIONS);
+	await app.listen(3000);
 }
 bootstrap();
